a6545c4816367bf31ba987b35c5bfcb7a5cb734b
aleciv26-bit <aleciv26@gmail.com>
2026-07-31 16:49:30 +0200

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -2,8 +2,6 @@ export type Dish = {
   id: number;
   image: string;
   name: string;
-  description: string;
-  price: string;
   tag?: string;
 };
 
@@ -11,34 +9,26 @@ export const dishes: Dish[] = [
   // --- SALATO ---
   {
     id: 1,
-    image: '/images/mezzelune-branzino.jpg', // Modifica il nome del file con il tuo reale
+    image: '/images/mezzelune-branzino.jpg',
     name: 'Mezzelune ripiene al branzino',
-    description: 'Pasta fresca ripiena di delicato branzino, servita con un condimento leggero.',
-    price: '16 €', // Inserisci il prezzo corretto
     tag: 'Salato',
   },
   {
     id: 2,
     image: '/images/saute-cozze-vongole.jpg',
     name: 'Sauté di cozze e vongole',
-    description: 'Cozze e vongole fresche saltate in padella con aglio, prezzemolo e crostini di pane.',
-    price: '14 €',
     tag: 'Salato',
   },
   {
     id: 3,
     image: '/images/spaghetti-scogliera.jpg',
     name: 'Spaghetti alla scogliera',
-    description: 'Spaghetti trafilati al bronzo con frutti di mare freschi e pomodorini sfumati al vino bianco.',
-    price: '18 €',
     tag: 'Salato',
   },
   {
     id: 4,
     image: '/images/zuppa-pesce.jpg',
     name: 'Zuppa di Pesce',
-    description: 'Ricca e gustosa zuppa con il pescato del giorno servita con crostini dorati.',
-    price: '20 €',
     tag: 'Salato',
   },
 
@@ -47,32 +37,24 @@ export const dishes: Dish[] = [
     id: 5,
     image: '/images/cheesecake-frutti-bosco.jpg',
     name: 'Cheesecake ai frutti di bosco',
-    description: 'Cremosa cheesecake artigianale arricchita con una salsa ai frutti di bosco freschi.',
-    price: '6 €',
     tag: 'Dessert',
   },
   {
     id: 6,
     image: '/images/mousse-tre-cioccolati.jpg',
     name: 'Mousse ai tre cioccolati',
-    description: 'Delicata mousse a tre strati: cioccolato fondente, al latte e bianco.',
-    price: '7 €',
     tag: 'Dessert',
   },
   {
     id: 7,
     image: '/images/souffle-cioccolato.jpg',
     name: 'Soufflé al cioccolato con cuore caldo',
-    description: 'Soffice tortino al cioccolato servito caldo con cuore cremoso e fondente.',
-    price: '7 €',
     tag: 'Dessert',
   },
   {
     id: 8,
     image: '/images/tiramisu.jpg',
     name: 'Tiramisù',
-    description: 'Il classico dessert italiano fatto in casa con crema al mascarpone e caffè.',
-    price: '6 €',
     tag: 'Dessert',
   },
 ];
